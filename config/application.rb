2b785e55e1882597e224accc5224216ca6ccef38
Kevin Bongart <contact@kevinbongart.net>
2018-05-21 09:11:21 -0400

    Email

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,18 @@ module EasyRSVP
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.action_mailer.default_url_options = { host: ENV['DOMAIN'] }
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.smtp_settings = {
+      user_name: ENV['SENDGRID_USERNAME'],
+      password: ENV['SENDGRID_PASSWORD'],
+      domain: ENV['DOMAIN'],
+      address: 'smtp.sendgrid.net',
+      port: 587,
+      authentication: :plain,
+      enable_starttls_auto: true
+    }
   end
 end
