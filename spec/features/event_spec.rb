921858850c07d107bebcbdea4bee17c7b31015c2
Kevin Bongart <contact@kevinbongart.net>
2021-01-01 14:35:33 +0100

    Fix date-specific spec. Happy new year!

--- spec/features/event_spec.rb
+++ spec/features/event_spec.rb
@@ -6,13 +6,14 @@ describe 'event creation' do
 
     fill_in 'What are you planning?', with: "Batman's surprise birthday"
 
-    select '2020', from: 'event_date_1i'
+    select Date.today.year, from: 'event_date_1i'
     select 'April', from: 'event_date_2i'
     select '7', from: 'event_date_3i'
 
     click_button 'Create your event, for free!'
 
-    expect(page).to have_content "Batman's surprise birthday // Tuesday, Apr 07 2020"
+    expect(page).to have_content "Batman's surprise birthday // "
+    expect(page).to have_content "Apr 07 #{Date.today.year}"
 
     fill_in 'Your email address:', with: 'alfred@wayne-enterprises.com'
     click_button 'Receive the admin link'
