f6ddacc6969607f2bb10283afdcd3704b944573c
Kevin Bongart <154600+KevinBongart@users.noreply.github.com>
2024-10-31 11:47:49 +0100

    schema for Rails 7.2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_20_075902) do
+ActiveRecord::Schema[7.2].define(version: 2023_06_20_075902) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -39,7 +39,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_20_075902) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.integer "blob_id", null: false
+    t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
