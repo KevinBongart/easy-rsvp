7507cf3ecec08f4dc20ad51dce8bca032e69d988
Kevin Bongart <154600+KevinBongart@users.noreply.github.com>
2026-09-19 19:48:26 +0200

    Document mailer railtie prerequisite

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ require "action_view/railtie"
 require "action_text/engine"
 
 # require "action_cable/engine"
+# require "action_mailer/railtie"
 # require "action_mailbox/engine"
 # require "rails/test_unit/railtie"
 
