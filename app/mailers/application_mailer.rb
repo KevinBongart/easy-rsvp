6de158f06a68e598a3d662b0d0766d0f958cbfac
Kevin Bongart <154600+KevinBongart@users.noreply.github.com>
2026-09-22 13:38:44 +0200

    Remove dormant email and unused dependencies (#902)
    * Remove dormant email and unused dependencies
    
    * Keep mailer configuration examples
    
    * Document mailer railtie prerequisite
    
    * Remove stale test mail reference

--- app/mailers/application_mailer.rb
+++
@@ -1,4 +0,0 @@
-class ApplicationMailer < ActionMailer::Base
-  default from: "Easy RSVP <info@easy-rsvp.com>"
-  layout 'mailer'
-end
--- app/mailers/user_mailer.rb
+++
@@ -1,6 +0,0 @@
-class UserMailer < ApplicationMailer
-  def admin_url_request_email(email_address, event)
-    @event = event
-    mail(to: email_address, subject: "Easy RSVP: #{event.title}")
-  end
-end
