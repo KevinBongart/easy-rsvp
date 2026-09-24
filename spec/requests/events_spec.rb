73b38cd16cae4ac4fb76ede2202641f871e01c00
Kevin Bongart <154600+KevinBongart@users.noreply.github.com>
2026-09-24 18:08:06 +0200

    Bypass Turbo for calendar downloads

--- spec/requests/events_spec.rb
+++ spec/requests/events_spec.rb
@@ -132,6 +132,8 @@ RSpec.describe 'Public events', type: :request do
     expect(page.at_css('.event-metadata > .fs-2.text-muted')).to be_present
     expect(page.at_css('.calendar-link svg.bi-calendar-plus[aria-hidden="true"]')).to be_present
     expect(page.at_css('.calendar-link')['href']).to eq(calendar_event_path(event, format: :ics))
+    expect(page.at_css('.calendar-link')['data-turbo']).to eq('false')
+    expect(page.at_css('.calendar-link')['data-turbo-prefetch']).to eq('false')
     expect(page.at_css('.calendar-link').parent['class'].split).to include('small')
     expect(page.at_css('.calendar-link')['class'].split).not_to include('text-muted')
     expect(response.body).not_to include(event.admin_token)
